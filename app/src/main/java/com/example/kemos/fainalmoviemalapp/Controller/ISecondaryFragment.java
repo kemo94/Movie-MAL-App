e80d290322da26f115467f7d2f43b22375564eaa
kemo94 <kareem.hussein94@yahoo.com>
2016-04-12 05:14:53 +0100

    fix some tablet bugs

---
+++ app/src/main/java/com/example/kemos/fainalmoviemalapp/Controller/ISecondaryFragment.java
@@ -0,0 +1,11 @@
+package com.example.kemos.fainalmoviemalapp.Controller;
+
+import com.example.kemos.fainalmoviemalapp.Model.MovieItem;
+
+/**
+ * Created by kemos on 4/9/2016.
+ */
+public interface ISecondaryFragment {
+
+     public void setMovieItem(MovieItem movieItem);
+}
---
+++ app/src/main/java/com/example/kemos/fainalmoviemalapp/Controller/SettingsActivity.java
@@ -0,0 +1,80 @@
+package com.example.kemos.fainalmoviemalapp.Controller;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.kemos.fainalmoviemalapp.R;
+
+public class SettingsActivity extends PreferenceActivity
+        implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+          addPreferencesFromResource(R.xml.pref_general);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+    }
+
+     private void bindPreferenceSummaryToValue(Preference preference) {
+         preference.setOnPreferenceChangeListener(this);
+       onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this,SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_favorite_list) {
+            startActivity(new Intent(this,FavoritesActivity.class));
+            return true;
+        }
+        if (id == R.id.action_watch_list) {
+            startActivity(new Intent(this,WatchListActivity.class));
+            return true;
+        }
+
+        if (id == R.id.action_activity_home) {
+            startActivity(new Intent(this,MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+             ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+             preference.setSummary(stringValue);
+        }
+        return true;
+    }
+
+}
